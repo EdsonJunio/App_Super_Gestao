c6338b96f093b7d4bf73d93e6967bbeff8d685eb
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-10-02 20:47:28 -0300

    Implementando o cadastro de fornecedores conclusao

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('app.fornecedor.index');
     }
 
@@ -15,8 +17,41 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar');
     }
 
-    public function adicionar()
+    public function adicionar(Request $request)
     {
-        return view('app.fornecedor.adicionar');
+        $msg = '';
+
+        if ($request->input('_token') != '') {
+            //validacao
+
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no mińimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mińimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no máximo 2 caracteres',
+                'email.max' => 'O campo e-mail não foi preenchido corretamente',
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            // redirect 
+
+            // dados view
+            $msg = 'Cadastro realizado com sucesso';
+
+
+        }
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
