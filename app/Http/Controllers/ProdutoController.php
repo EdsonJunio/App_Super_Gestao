fb726b980d81a3e63e4df5938bca6a5ecc5fb6fe
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-10-06 12:29:07 -0300

    Controladores com resources

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,11 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Produto;
+use Response;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    public function index() {
-        return view('app.produto');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Produto  $produto
+     * @return Response
+     */
+    public function show(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request  $request
+     * @param Produto  $produto
+     * @return Response
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Produto  $produto
+     * @return Response
+     */
+    public function destroy(Produto $produto)
+    {
+        //
     }
 }
